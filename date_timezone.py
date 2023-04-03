16a583e0af1b8c094493f35e5b6a6010d0dabec6
sai teja thalari <127551976+naruto1212121111111@users.noreply.github.com>
2023-04-02 19:01:12 -0500

    Update date_timezone.py

--- date_timezone.py
+++ date_timezone.py
@@ -1,19 +1,18 @@
 import datetime
 import pytz
 
-# Input datetime string
-datetime_str = "2023-03-11 15:30:00"
 
-# Create a datetime object from the string
-datetime_obj = datetime.datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+def transform_datetime(datetime_str, original_tz, target_tz):
+    datetime_obj = datetime.datetime.strptime(datetime_str,
+                                              "%Y-%m-%d %H:%M:%S")
+    original_timezone = pytz.timezone(original_tz)
+    datetime_obj = original_timezone.localize(datetime_obj)
+    target_timezone = pytz.timezone(target_tz)
+    target_datetime = datetime_obj.astimezone(target_timezone)
+    target_datetime = target_timezone.normalize(target_datetime)
+    updated_datetime = target_datetime.strftime("%m/%d/%Y %A %H:%M")
+    print(updated_datetime)  # add this line to display the formatted datetime
+    return updated_datetime
 
-# Set the timezone of the datetime object to the original timezone
-original_timezone = pytz.timezone("US/Pacific")
-datetime_obj = original_timezone.localize(datetime_obj)
 
-# Convert the datetime object to a different timezone
-target_timezone = pytz.timezone("Asia/Kolkata")
-target_datetime = datetime_obj.astimezone(target_timezone)
-
-# Print the target datetime in the desired format
-print(target_datetime.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+transform_datetime("2023-03-11 16:30:00", "US/Pacific", "Asia/Kolkata")
