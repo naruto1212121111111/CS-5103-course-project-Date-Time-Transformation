0f32b0bffb6132a750a5cec63bc642a5991bc77b
sai teja thalari <127551976+naruto1212121111111@users.noreply.github.com>
2023-04-30 01:24:10 -0500

    Update date_timezone.py

--- date_timezone.py
+++ date_timezone.py
@@ -2,17 +2,21 @@ import datetime
 import pytz
 
 
-def transform_datetime(datetime_str, original_tz, target_tz):
+def transform_datetime(datetime_str, original_tz, target_tz, apply_dst=True):
     datetime_obj = datetime.datetime.strptime(datetime_str,
                                               "%Y-%m-%d %H:%M:%S")
     original_timezone = pytz.timezone(original_tz)
-    datetime_obj = original_timezone.localize(datetime_obj)
+    datetime_obj = original_timezone.localize(datetime_obj, is_dst=apply_dst)
     target_timezone = pytz.timezone(target_tz)
     target_datetime = datetime_obj.astimezone(target_timezone)
+
+    if apply_dst:
+        target_datetime = target_datetime - target_timezone.dst(
+            datetime_obj.replace(tzinfo=None))
     target_datetime = target_timezone.normalize(target_datetime)
     updated_datetime = target_datetime.strftime("%m/%d/%Y %A %H:%M")
-    print(updated_datetime)  # add this line to display the formatted datetime
+    print(updated_datetime)
     return updated_datetime
 
 
-transform_datetime("2023-03-11 16:30:00", "US/Pacific", "Asia/Kolkata")
+transform_datetime("2023-11-01 16:30:00", "US/Pacific", "Asia/Kolkata")
